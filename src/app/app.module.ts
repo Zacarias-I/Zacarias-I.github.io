e69a60682dd0246674af096d1b3fc88f96c472b9
Zacarias <zacarillomz9@gmail.com>
2023-09-12 21:19:18 -0300

    AAAAAAAA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaComponent } from './tela/tela.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaComponent
   ],
   imports: [
     BrowserModule,
