32b0d2e8d5acac0c6af0b4454a6854f3af261bcd
Zacarias <zacarillomz9@gmail.com>
2023-09-19 20:51:25 -0300

    ta funfas

--- src/app/pega.service.ts
+++ src/app/pega.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import { tap, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PegaService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  
+  load() {
+    let feed = localStorage.getItem('_feed');
+
+    if ( !feed )
+      return this.http.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fsegurancalegal.com%2Ffeed%3Fpaged%3D1')
+        .pipe(
+          tap((_feed) => {
+            localStorage.setItem('_feed', JSON.stringify(_feed));
+          })
+        )
+
+    return of(JSON.parse(feed));
+  }
 }
--- src/app/tela/tela.component.ts
+++ src/app/tela/tela.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { PegaService } from '../pega.service';
 
 @Component({
   selector: 'app-tela',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./tela.component.css']
 })
 export class TelaComponent {
+  data: any = {};
+  @ViewChild('player') player?: ElementRef;
+  playing: any = {};
 
+  constructor(
+    private feed: PegaService,
+  ) {}
+  
+  ngOnInit() {
+    this.feed.load()
+      .subscribe({
+        next: (feed) => {
+          this.data = feed;
+          console.log(feed);
+        },
+        error: (response) => {
+          console.error(response);
+        },
+        complete: () => {
+          console.log("complete");
+        }
+      })
+  }
+
+  play(item: any) {
+    const element = this.player?.nativeElement;
+    this.stop();
+    element.src = item.enclosure.link;
+    element.play();
+    this.playing = item.enclosure;
+  }
+
+  stop() {
+    const element = this.player?.nativeElement;
+    element.pause();
+    element.currentTime = 0;
+    element.src = "";
+    this.playing = {};
+  }
 }
